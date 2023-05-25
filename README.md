ce3da902c1c50449fc1b47494cc13b7529874771
ddy <58058861+ddy-ddy@users.noreply.github.com>
2023-05-25 18:02:09 +0800

    feat: edit the readme

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# TOEFL-TPO
-The resources of TOEFL TPO
+## TOEFL-TPO
+
+- 该仓库收集了托福`tpo 30~54`的阅读真题
+- 阅读的原始数据保存在`data/json`文件夹中，可供任意操作
+- 阅读的epub格式保存在`data/epub`文件夹中，可导入到阅读编辑器中
+
+## JSON数据格式
+- 原始数据存储为json格式，每个字典代表一篇文章
+- 每篇文章的每个段落保存在`article`字段中
+```json
+[
+  {
+    "id": "tpo-30-1",                      //TPO30 第一篇阅读
+    "title": "",                           //该篇文章标题
+    "link": "",                            //考满分对应练习页面链接
+    "article": [
+      "A heated debate has enlivened...",  //该篇文章第一段
+      "A heated debate has enlivened...",  //该篇文章第二段
+      "A heated debate has enlivened..." 
+    ]
+  },
+  {
+    "id": "tpo-30-2", //TPO30 第二篇阅读
+    "title": "",
+    "link": "",
+    "article": [
+    ]
+  },
+  {
+    "id": "tpo-30-3", //TPO30 第三篇阅读
+    "title": "",
+    "link": "",
+    "article": [
+    ]
+  }
+]
+```
+## epub数据格式
+- epub格式的数据可以很方便的导入到各种电子表格中，方便大家的阅读
+- 运行`src/text2epub.py`可以将指定的json数据转换为epub电子书的格式
+<img src="https://ddy-1310349779.cos.ap-shanghai.myqcloud.com/typora/%E6%88%AA%E5%B1%8F2023-05-25%2017.57.59.jpg" style="zoom:50%;" />
--- src/text2epub.py
+++ src/text2epub.py
@@ -1,15 +1,19 @@
 '''
-Author: ddy-ddy
-Date: 2023-05-24 14:00:52
-LastEditTime: 2023-05-24 21:14:20
-Github: https://github.com/ddy-ddy
-Website: https://ddy-ddy.com
+将指定的文本/数据转换为epub格式
 '''
+
 from ebooklib import epub
 import json
 
 
 def create_epub(original_data_path, epub_path, epub_title):
+    '''
+
+    :param original_data_path: 数据所在的路径
+    :param epub_path: epub文件所在的路径
+    :param epub_title: epub文件标题
+    :return:
+    '''
     # 读取json数据
     with open(original_data_path, 'r') as f:
         data = json.load(f)
@@ -44,12 +48,12 @@ def create_epub(original_data_path, epub_path, epub_title):
     book.add_item(epub.EpubNcx())
 
     # 设置封面
-    book.set_cover('cover.jpg', open('../static/cover-1.jpg', 'rb').read())
+    book.set_cover('cover.jpg', open('../static/cover-5.jpg', 'rb').read())
 
     # 生成epub文件
     epub.write_epub(epub_path, book, {})
 
 
 if __name__ == '__main__':
-    create_epub("../data/json/tpo-30-35.json", "../data/epub/TOEFL TPO 30~35.epub",
-                "TOEFL TPO 30~35")
+    create_epub("../data/json/tpo-51-54.json", "../data/epub/TOEFL TPO 51~54.epub",
+                "TOEFL TPO 51~54")
